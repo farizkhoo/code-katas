82f39aea5b0007d1d16dd1029fd02ee8705cd43e
farizkhoo <fariz_khoo@hotmail.com>
2018-06-11 01:54:24 +0800

    Add files via upload

--- day13.rb
+++ day13.rb
@@ -3,28 +3,19 @@ def MaximalSquare(strArr)
 	# display board
 	puts strArr
 	
-	# convert string array into integer matrix
+	result = 0
+	# convert string array into integer matrix and find largest square
 	strArr.each_with_index do |row, i|
 		strArr[i] = row.split(//)
 		strArr[i].each_with_index do |column, j|
 			strArr[i][j] = column.to_i
-		end
-	end
-	
-	# clone matrix
-	cache = strArr
-	result = 0
-
-	# find largest square using bottom-right index
-	strArr.each_with_index do |row, i|
-		strArr[i].each_with_index do |column, j|
 			if i == 0 || j == 0
 			elsif strArr[i][j] > 0
 				# checks values up, left, and up-left
-				cache[i][j] = 1 + [cache[i][j-1], cache[i-1][j], cache[i-1][j-1]].min
+				strArr[i][j] = 1 + [strArr[i][j-1], strArr[i-1][j], strArr[i-1][j-1]].min
 			end
-			if cache[i][j] > result
-				result = cache[i][j]
+			if strArr[i][j] > result
+				result = strArr[i][j]
 			end
 		end
 	end
